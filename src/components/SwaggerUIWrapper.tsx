02a0b0353df2b6917026590b3c63dc2456821065
Matteo Roggia <matteo.roggia@gmail.com>
2025-06-03 15:45:29 +0200

    fix: made swaggerui a client only component to allow ssr render

---
+++ src/components/SwaggerUIWrapper.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+
+export default function SwaggerUIWrapper() {
+  const [SwaggerUI, setSwaggerUI] = useState<any>(null);
+
+  useEffect(() => {
+    import('swagger-ui-react').then((module) => {
+      setSwaggerUI(() => module.default);
+      import('swagger-ui-react/swagger-ui.css'); // Load CSS dynamically
+      import('@site/src/css/swagger-dark-fix.css');
+    });
+  }, []);
+
+  if (!SwaggerUI) return <div>Loading Swagger UI...</div>;
+
+  return (
+    <SwaggerUI url="https://mobility.api.opendatahub.com/v2/apispec" tryItOutEnabled={true} />
+  );
+}
