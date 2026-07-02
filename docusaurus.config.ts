b95d819cb9a5db4737ffd86902d2c81dd9fde93a
Matteo Roggia <matteo.roggia@gmail.com>
2026-07-02 10:51:52 +0200

    chore: prod cicd

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'http://localhost:3000',
+  url: process.env.DOCS_URL || 'http://localhost:3000',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
